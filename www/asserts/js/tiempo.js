8e1839d80b65e4754ddc8009ae7d456a093b0e8e
Alejandro Parra Purroy <Alex.Parra.Purroy@gmail.com>
2021-10-21 12:36:30 +0200

    V2 Definitiva

--- www/asserts/js/tiempo.js
+++ www/asserts/js/tiempo.js
@@ -42,13 +42,40 @@ function tiempoDe5Dias() {
     $("#result").html(result);
   });
 }
+var onSuccess = function (position) {
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    position.coords.latitude +
+    "&lon=" +
+    position.coords.longitude +
+    "&appid=7490ba3dbd45700ca1a91b4328446781";
+  $.ajax({
+    url: url,
+    type: "GET",
+  }).done(function (response) {
+    var result = "";
+    var tiempo = response.weather[0];
+    result +=
+      "<div class='card'><div class='row g-0'><div class='col-5 col-sm-4'><img src='http://openweathermap.org/img/wn/" +
+      tiempo.icon +
+      "@2x.png'></div><div class='col-7 col-sm-8'><div class='card-body><h1 class='card-title'>" +
+      tiempo.description +
+      "</h1></div></div></div></div>";
+    $("#results").html(result);
+  });
+};
+function onError(error) {
+  alert("code: " + error.code + "\n" + "message: " + error.message + "\n");
+}
 $(function () {
   $("#search").hide();
+  $("#location").hide();
   $(".nav-item")
     .first()
     .on("click", function () {
       $("#search").hide();
       $("#main").show();
+      $("#location").hide();
     });
   $(".nav-item")
     .first()
@@ -56,6 +83,17 @@ $(function () {
     .on("click", function () {
       $("#search").show();
       $("#main").hide();
+      $("#location").hide();
+    });
+  $(".nav-item")
+    .first()
+    .next()
+    .next()
+    .on("click", function () {
+      $("#search").hide();
+      $("#main").hide();
+      $("#location").show();
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
     });
   $("#btnundia").on("click", function () {
     tiempoDelDia();
